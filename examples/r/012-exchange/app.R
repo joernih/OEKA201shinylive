a2a60015227887dfaafa4ad1120a3e3974b043d2
joernih <jorn.halvorsen@gmail.com>
2024-04-01 18:12:22 +0200

    exc

---
+++ examples/r/012-exchange/app.R
@@ -0,0 +1,49 @@
+webr::install("OEKA201WASMP", repos = "https://joernih.github.io/OEKA201WASMA/")
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library("OEKA201WASMP")
+### **Gjøre i stand dataene om pengemengdeveksten fra FRED**
+data_vk <- data.frame(OEKA201WASMP::nb_ts[[1]])
+ui <- fluidPage(
+  titlePanel("Mange land"),
+  sidebarLayout(
+   sidebarPanel(
+       selectInput("veks","Form",c("value_EUR","value_USD")),
+       textInput("datef", "Select fra dato:", value = "2020-01-01"),
+       textInput("datet", "Select til dato:", value = "2021-10-05")
+    ),
+   mainPanel(
+     tabsetPanel(
+       tabPanel("Plot",plotOutput("plot")),
+       tabPanel("Tabell",tableOutput("table"))
+            )
+    )
+  )
+)
+server <- function(input, output) {
+  res <- reactive({
+    veks <- input$veks
+    startd_i <- as.Date(input$datef)
+    startd_n <- as.Date(input$datet)
+    mvg <- data_vk %>% dplyr::mutate(date=base::as.Date(dato)) %>%
+     # Filter out all observations starting earlier or equal than startd_i
+     dplyr::filter(date>startd_i) %>%
+     # Filter out all observations starting later or equal than startd_n
+     dplyr::filter(date<startd_n) 
+    list(mvg,veks)
+  })
+  output$plot <- renderPlot({
+    ggplot2::ggplot(res()[[1]], aes(x = date, y =!!as.name(res()[[2]]))) + 
+      ggplot2::geom_point() +
+      ggplot2::labs(x = "Date", y = "Exchange Rate", color = "Currency") +
+      ggplot2::theme_classic()
+
+  })
+  output$table <- renderTable({
+    res()[[1]]
+  })
+}
+shinyApp(ui = ui, server = server)
+
+
